936f4e023e42a3987c536745d5732ab41580eefd
pleroldan <pl.roldan1@gmail.com>
2025-06-17 16:32:18 -0300

    Se agrega informacion nodered

---
+++ C- Prototipos/Bases/Modulo, 2 lora punto a punto + mqtt/ESP32_gy302_Emisor/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/plr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/plr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/plr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/plr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
